28c96dc43a6f4125c3f3ab8f5ccf0408187a9b11
Nick Stalter <nstalter@cars.com>
2025-10-23 10:39:56 -0400

    remove page controller

--- test/git_colors_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule GitColorsWeb.PageControllerTest do
-  use GitColorsWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
-  end
-end
